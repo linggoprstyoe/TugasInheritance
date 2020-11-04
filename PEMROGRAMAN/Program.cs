632702823715715c1a1a09a6cf0eded340774a04
linggoprstyoe <61672630+linggoprstyoe@users.noreply.github.com>
2020-11-04 16:31:53 +0700

    Add files via upload

---
+++ PEMROGRAMAN/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace tugas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Monokotil kelapa = new Monokotil("Jagung",1);
+           kelapa.Ciri();
+        }
+    }
+    class Tumbuhan
+    {
+        public int keping;
+        
+        public Tumbuhan(){
+           
+        }
+        public void berbuahk(){
+            Console.WriteLine("Matang");
+        }
+
+    }
+    class Monokotil:Tumbuhan{
+        public string nama;
+        public Monokotil(string nama_tumbhn, int jmlh_keping ){
+            nama = nama_tumbhn;
+            keping = jmlh_keping;
+            
+        }
+        public void Ciri(){
+            Console.WriteLine("Jumlah keping: " + keping +" nama: " + nama );
+        }
+    }
+}
